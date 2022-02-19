9848990170463b57ebf9a9d6c153d7c26b6bd30f
Eniola Ajayi <ajayieniola16@gmail.com>
2022-02-19 10:06:27 +0100

    add tetris spawining functionality

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -7,11 +7,23 @@ public class Piece : MonoBehaviour
     public Board board {get; private set;}
     public TetrominoData data {get; private set;}
     public Vector3Int position {get; private set;}
+    public Vector3Int[] cells {get; private set;}
     // spawn position
     // tetromino data to use while pieceis active
     // and reference to the game board 
     public void Initialize(Board board,Vector2Int position, TetrominoData data)
     {
+        this.board = board;
+        this.position = (Vector3Int)position;
+        this.data = data;
 
+        if(this.cells == null){
+            this.cells = new Vector3Int[data.cells.Length];
+        }
+
+        for (int i = 0; i < data.cells.Length; i++)
+        {
+            this.cells[i] = (Vector3Int)data.cells[i];
+        }
     }
 }
